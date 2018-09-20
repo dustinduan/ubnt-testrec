a90923b87ec92f9c68bd63241cbd63ecc65196fb
dustinduan2010 <dustinduan2010@163.com>
2018-09-20 13:26:13 +0800

    版本升级，在网络通讯允许的情况下，自动压缩本机和拂去其上面的所有备份文件

--- datazhengli.py
+++ datazhengli.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 def txt_compress(filename):
     df=[]
     with open(filename,'r') as source:
@@ -8,6 +9,17 @@ def txt_compress(filename):
     with open('temp.txt','w') as tar:
         for i in k:
             tar.write(i)
-    os.remove(filename)
-    os.rename('temp.txt',filename)
-txt_compress('test.txt')
+    f1=open('temp.txt')
+    f2=open(filename,'w')
+    shutil.copyfileobj(f1,f2)
+    f1.close()
+    f2.close()
+    os.remove('temp.txt')
+filelist=['test.txt','//10.6.1.14/公用Public/test.txt','//10.6.1.14/Assembly Public/testrec/test.txt','d:/summery.txt']
+for i in filelist:
+    if os.path.exists(i):
+        try:
+            txt_compress(i)
+        except:
+            pass
+print('log文件压缩完成')
